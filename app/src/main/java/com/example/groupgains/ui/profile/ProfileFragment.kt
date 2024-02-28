5235e2594cba6dacff8c0c5bedef2a082dacf28a
Joshua Kurien <joshua.kurien4@gmail.com>
2024-02-28 11:46:05 -0500

    Added record fragment initial setup

--- app/src/main/java/com/example/groupgains/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/groupgains/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.groupgains.ui.record
+package com.example.groupgains.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.groupgains.databinding.FragmentRecordBinding
+import com.example.groupgains.databinding.FragmentProfileBinding
 
-class RecordFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentRecordBinding? = null
+    private var _binding: FragmentProfileBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class RecordFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val recordViewModel =
-            ViewModelProvider(this).get(RecordViewModel::class.java)
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
-        _binding = FragmentRecordBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textRecord
-        recordViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/groupgains/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/groupgains/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.groupgains.ui.record
+package com.example.groupgains.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class RecordViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is record Fragment"
+        value = "This is profile Fragment"
     }
     val text: LiveData<String> = _text
 }
