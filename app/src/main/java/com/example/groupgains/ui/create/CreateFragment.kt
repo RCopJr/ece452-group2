a6ca80770b648c49e469fbd962bd3ac6f66dbd71
Joshua Kurien <joshua.kurien4@gmail.com>
2024-02-28 11:41:26 -0500

    Added profile and create pages

--- app/src/main/java/com/example/groupgains/ui/create/CreateFragment.kt
+++ app/src/main/java/com/example/groupgains/ui/create/CreateFragment.kt
@@ -1,4 +1,4 @@
-package com.example.groupgains.ui.notifications
+package com.example.groupgains.ui.create
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.groupgains.databinding.FragmentNotificationsBinding
+import com.example.groupgains.databinding.FragmentCreateBinding
 
-class NotificationsFragment : Fragment() {
+class CreateFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentCreateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val createViewModel =
+            ViewModelProvider(this).get(CreateViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textCreate
+        createViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
