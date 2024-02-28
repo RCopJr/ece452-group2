1ea8235f12ff6a7aededa00b1dbd0dc41ac0f697
Joshua Kurien <joshua.kurien4@gmail.com>
2024-02-28 13:14:37 -0500

    Added record fragment in nav and changed theme

--- app/src/main/java/com/example/groupgains/MainActivity.kt
+++ app/src/main/java/com/example/groupgains/MainActivity.kt
@@ -26,7 +26,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_create, R.id.navigation_profile
+                R.id.navigation_home,
+                R.id.navigation_record,
+                R.id.navigation_create,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
