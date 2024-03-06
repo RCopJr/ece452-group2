53e725085bc988f20f3af854d24790b8282ab035
Jessica Wormald <jessica.wormald@emetros.com>
2024-03-05 22:02:20 -0500

    updated stopwatch

--- app/src/main/java/com/example/groupgains/ui/record/RecordFragment.kt
+++ app/src/main/java/com/example/groupgains/ui/record/RecordFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.groupgains.R
 import com.example.groupgains.databinding.FragmentRecordBinding
+
 
 class RecordFragment : Fragment() {
 
