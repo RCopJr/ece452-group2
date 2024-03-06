7623d979b4c9a9c8691b7f17fb183dfeca2ac276
Samuel Diaz Sevillano <sediazse@uwaterloo.ca>
2024-03-06 09:13:20 -0500

    Workout list temporal implementation

--- app/src/main/java/com/example/groupgains/ui/record/RecordOne.kt
+++ app/src/main/java/com/example/groupgains/ui/record/RecordOne.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.groupgains.R
@@ -23,17 +25,35 @@ class RecordOne: Fragment() {
         _binding = Record1Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val butt = binding.button11
+        val buttonContain: LinearLayout = binding.buttonContainer
+        buttonContain.setPadding(150,0,0,0)
+
 
         val rec2 = RecordTwo()
-
-        butt.setOnClickListener {
-            parentFragmentManager.beginTransaction().apply {
-                replace(R.id.frame, rec2)
-                commit()
-            }
-        }
+        addButtons(5, buttonContain, rec2)
 
         return root
     }
+
+    private fun addButtons(data: Int, container: LinearLayout, page: RecordTwo) {
+        val workouts = listOf<String>("Leg day", "Chest Day", "Arm Day", "Push-pull",
+            "Workout", "Workout", "Workout","Workout", "Workout", "Workout")
+        for (i in 1..data){
+            val button = Button(requireContext())
+            button.text = workouts[i-1]
+
+            val params = ViewGroup.LayoutParams(750, 250)
+            button.layoutParams = params
+
+            container.addView(button)
+
+            button.setOnClickListener {
+                parentFragmentManager.beginTransaction().apply {
+                    replace(R.id.frame, page)
+                    commit()
+                }
+            }
+        }
+    }
+
 }
