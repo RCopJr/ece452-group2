140949167b2834f07fcbc99439874eeb63ca1461
Samuel Diaz Sevillano <sediazse@uwaterloo.ca>
2024-03-27 20:09:53 -0400

    Create UI workflow

--- app/src/main/java/com/example/groupgains/ui/create/CreateTwo.kt
+++ app/src/main/java/com/example/groupgains/ui/create/CreateTwo.kt
@@ -11,8 +11,11 @@ import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet.Constraint
 import androidx.fragment.app.Fragment
+import com.example.groupgains.R
 
 import com.example.groupgains.databinding.Create2Binding
+import com.example.groupgains.databinding.CreateWorkoutDetailsBinding
+import com.example.groupgains.databinding.CreateWorkoutListBinding
 
 
 class CreateTwo: Fragment() {
@@ -28,7 +31,41 @@ class CreateTwo: Fragment() {
         _binding = Create2Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val buttonC = binding.buttonCancel
+        val buttonA = binding.buttonAdd
+        val buttonS = binding.buttonSave
+
+        val contain = binding.workoutsEditionContainer
+
+
+        buttonC.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.frame, CreateOne())
+                commit()
+            }
+        }
+
+        buttonA.setOnClickListener {
+            add_exercise(contain, inflater)
+        }
+
+        buttonS.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.frame, CreateOne())
+                commit()
+            }
+        }
 
         return root
+    }
+
+    fun add_exercise(container: LinearLayout, inflater: LayoutInflater) {
+        val binding2 = CreateWorkoutDetailsBinding.inflate(inflater, container, false)
+        container.addView(binding2.root)
+
+        val buttonD = binding2.buttonDel
+        buttonD.setOnClickListener {
+            container.removeView(binding2.root)
+        }
     }
 }
