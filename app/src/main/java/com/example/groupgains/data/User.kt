9ee55e84a97f7f2460caf9fc5e79ac0ddd455b9b
Samuel Diaz Sevillano <sediazse@uwaterloo.ca>
2024-04-03 00:33:30 -0400

    Fixed bug in User class

--- app/src/main/java/com/example/groupgains/data/User.kt
+++ app/src/main/java/com/example/groupgains/data/User.kt
@@ -4,5 +4,5 @@ data class User(
     val userName: String = "",
     val user_id: String = "",
     val friends: MutableList<String> = mutableListOf(),
-    val color: Int,
+    val color: Int = 2131165357, //Default is green *2131165357*
 )
